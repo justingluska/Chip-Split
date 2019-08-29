3b18038da1c2a63911d4b1050b98c7addde9d1c8
Justin Gluska <justingluska@gmail.com>
2019-08-28 22:19:36 -0400

    Finished!

--- Chip Split/ResultsViewController.swift
+++ Chip Split/ResultsViewController.swift
@@ -13,6 +13,8 @@ class ResultsViewController: UIViewController {
     override func viewDidLoad() {
         genBo.layer.cornerRadius = 15
         backBo.layer.cornerRadius = 15
+        textOutputView.layer.cornerRadius = 15
+        textOutputView.clipsToBounds = true
         super.viewDidLoad()
     }
     
@@ -62,6 +64,7 @@ class ResultsViewController: UIViewController {
         totalPlayers.text = "Total Players: \(tPl)"
     }
     
+    @IBOutlet weak var textOutputView: UITextView!
     @IBOutlet weak var totalPlayers: UILabel!
     @IBOutlet weak var results: UITextView!
     
@@ -88,59 +91,79 @@ class ResultsViewController: UIViewController {
             results.text = "\(chipQuantity1/tPl) \(chipName1.uppercased()) \(totalValue1)\n\(chipQuantity2/tPl) \(chipName2.uppercased()) \(totalValue2)\n\(chipQuantity3/tPl) \(chipName3.uppercased()) \(totalValue3)"
         }
         if(totalTypes == 4){
-            var totalValue:Int = 1
-            totalValue = chipQuantity1 * chipAmount1
-            totalValue = (totalValue) + chipQuantity2 * chipAmount2
-            totalValue = (totalValue) + chipQuantity3 * chipAmount3
-            totalValue = (totalValue) + chipQuantity4 * chipAmount4
-            totalValue = totalValue / tPl
-            results.text = "\(chipQuantity1/tPl) \(chipName1.uppercased()) CHIPS EACH\n\(chipQuantity2/tPl) \(chipName2.uppercased()) CHIPS EACH\n\(chipQuantity3/tPl) \(chipName3.uppercased()) CHIPS EACH\n\(chipQuantity4/tPl) \(chipName4.uppercased()) CHIPS EACH \n\nINDIVIDUAL VALUE: $\(totalValue)"
+            var totalValue1:String = ""
+            totalValue1 = "CHIPS @ $\(chipAmount1) EACH"
+            var totalValue2:String = ""
+            totalValue2 = "CHIPS @ $\(chipAmount2) EACH"
+            var totalValue3:String = ""
+            totalValue3 = "CHIPS @ $\(chipAmount3) EACH"
+            var totalValue4:String = ""
+            totalValue3 = "CHIPS @ $\(chipAmount4) EACH"
+            results.text = "\(chipQuantity1/tPl) \(chipName1.uppercased()) \(totalValue1)\n\(chipQuantity2/tPl) \(chipName2.uppercased()) \(totalValue2)\n\(chipQuantity3/tPl) \(chipName3.uppercased()) \(totalValue3)\n\(chipQuantity4/tPl) \(chipName4.uppercased()) \(totalValue4)"
         }
         if(totalTypes == 5){
-            var totalValue:Int = 1
-            totalValue = chipQuantity1 * chipAmount1
-            totalValue = (totalValue) + chipQuantity2 * chipAmount2
-            totalValue = (totalValue) + chipQuantity3 * chipAmount3
-            totalValue = (totalValue) + chipQuantity4 * chipAmount4
-            totalValue = (totalValue) + chipQuantity5 * chipAmount5
-            totalValue = totalValue / tPl
-            results.text = "\(chipQuantity1/tPl) \(chipName1.uppercased()) CHIPS EACH\n\(chipQuantity2/tPl) \(chipName2.uppercased()) CHIPS EACH\n\(chipQuantity3/tPl) \(chipName3.uppercased()) CHIPS EACH\n\(chipQuantity4/tPl) \(chipName4.uppercased()) CHIPS EACH\n\(chipQuantity5/tPl) \(chipName5.uppercased()) CHIPS EACH \n\nINDIVIDUAL VALUE: $\(totalValue)"
+            var totalValue1:String = ""
+            totalValue1 = "CHIPS @ $\(chipAmount1) EACH"
+            var totalValue2:String = ""
+            totalValue2 = "CHIPS @ $\(chipAmount2) EACH"
+            var totalValue3:String = ""
+            totalValue3 = "CHIPS @ $\(chipAmount3) EACH"
+            var totalValue4:String = ""
+            totalValue4 = "CHIPS @ $\(chipAmount4) EACH"
+            var totalValue5:String = ""
+            totalValue5 = "CHIPS @ $\(chipAmount5) EACH"
+            results.text = "\(chipQuantity1/tPl) \(chipName1.uppercased()) \(totalValue1)\n\(chipQuantity2/tPl) \(chipName2.uppercased()) \(totalValue2)\n\(chipQuantity3/tPl) \(chipName3.uppercased()) \(totalValue3)\n\(chipQuantity4/tPl) \(chipName4.uppercased()) \(totalValue4)\n\(chipQuantity5/tPl) \(chipName5.uppercased()) \(totalValue5)"
         }
         if(totalTypes == 6){
-            var totalValue:Int = 1
-            totalValue = chipQuantity1 * chipAmount1
-            totalValue = (totalValue) + chipQuantity2 * chipAmount2
-            totalValue = (totalValue) + chipQuantity3 * chipAmount3
-            totalValue = (totalValue) + chipQuantity4 * chipAmount4
-            totalValue = (totalValue) + chipQuantity5 * chipAmount5
-            totalValue = (totalValue) + chipQuantity6 * chipAmount6
-            totalValue = totalValue / tPl
-            results.text = "\(chipQuantity1/tPl) \(chipName1.uppercased()) CHIPS EACH\n\(chipQuantity2/tPl) \(chipName2.uppercased()) CHIPS EACH\n\(chipQuantity3/tPl) \(chipName3.uppercased()) CHIPS EACH\n\(chipQuantity4/tPl) \(chipName4.uppercased()) CHIPS EACH\n\(chipQuantity5/tPl) \(chipName5.uppercased()) CHIPS EACH\n\(chipQuantity6/tPl) \(chipName6.uppercased()) CHIPS EACH \n\nINDIVIDUAL VALUE: $\(totalValue)"
+            var totalValue1:String = ""
+            totalValue1 = "CHIPS @ $\(chipAmount1) EACH"
+            var totalValue2:String = ""
+            totalValue2 = "CHIPS @ $\(chipAmount2) EACH"
+            var totalValue3:String = ""
+            totalValue3 = "CHIPS @ $\(chipAmount3) EACH"
+            var totalValue4:String = ""
+            totalValue4 = "CHIPS @ $\(chipAmount4) EACH"
+            var totalValue5:String = ""
+            totalValue5 = "CHIPS @ $\(chipAmount5) EACH"
+            var totalValue6:String = ""
+            totalValue6 = "CHIPS @ $\(chipAmount6) EACH"
+            results.text = "\(chipQuantity1/tPl) \(chipName1.uppercased()) \(totalValue1)\n\(chipQuantity2/tPl) \(chipName2.uppercased()) \(totalValue2)\n\(chipQuantity3/tPl) \(chipName3.uppercased()) \(totalValue3)\n\(chipQuantity4/tPl) \(chipName4.uppercased()) \(totalValue4)\n\(chipQuantity5/tPl) \(chipName5.uppercased()) \(totalValue5)\n\(chipQuantity6/tPl) \(chipName6.uppercased()) \(totalValue6)"
         }
         if(totalTypes == 7){
-            var totalValue:Int = 1
-            totalValue = chipQuantity1 * chipAmount1
-            totalValue = (totalValue) + chipQuantity2 * chipAmount2
-            totalValue = (totalValue) + chipQuantity3 * chipAmount3
-            totalValue = (totalValue) + chipQuantity4 * chipAmount4
-            totalValue = (totalValue) + chipQuantity5 * chipAmount5
-            totalValue = (totalValue) + chipQuantity6 * chipAmount6
-            totalValue = (totalValue) + chipQuantity7 * chipAmount7
-            totalValue = totalValue / tPl
-            results.text = "\(chipQuantity1/tPl) \(chipName1.uppercased()) CHIPS EACH\n\(chipQuantity2/tPl) \(chipName2.uppercased()) CHIPS EACH\n\(chipQuantity3/tPl) \(chipName3.uppercased()) CHIPS EACH\n\(chipQuantity4/tPl) \(chipName4.uppercased()) CHIPS EACH\n\(chipQuantity5/tPl) \(chipName5.uppercased()) CHIPS EACH\n\(chipQuantity6/tPl) \(chipName6.uppercased()) CHIPS EACH\n\(chipQuantity7/tPl) \(chipName7.uppercased()) CHIPS EACH \n\nINDIVIDUAL VALUE: $\(totalValue)"
+            var totalValue1:String = ""
+            totalValue1 = "CHIPS @ $\(chipAmount1) EACH"
+            var totalValue2:String = ""
+            totalValue2 = "CHIPS @ $\(chipAmount2) EACH"
+            var totalValue3:String = ""
+            totalValue3 = "CHIPS @ $\(chipAmount3) EACH"
+            var totalValue4:String = ""
+            totalValue4 = "CHIPS @ $\(chipAmount4) EACH"
+            var totalValue5:String = ""
+            totalValue5 = "CHIPS @ $\(chipAmount5) EACH"
+            var totalValue6:String = ""
+            totalValue6 = "CHIPS @ $\(chipAmount6) EACH"
+            var totalValue7:String = ""
+            totalValue7 = "CHIPS @ $\(chipAmount7) EACH"
+            results.text = "\(chipQuantity1/tPl) \(chipName1.uppercased()) \(totalValue1)\n\(chipQuantity2/tPl) \(chipName2.uppercased()) \(totalValue2)\n\(chipQuantity3/tPl) \(chipName3.uppercased()) \(totalValue3)\n\(chipQuantity4/tPl) \(chipName4.uppercased()) \(totalValue4)\n\(chipQuantity5/tPl) \(chipName5.uppercased()) \(totalValue5)\n\(chipQuantity6/tPl) \(chipName6.uppercased()) \(totalValue6)\n\(chipQuantity7/tPl) \(chipName7.uppercased()) \(totalValue7)"
         }
         if(totalTypes == 8){
-            var totalValue:Int = 1
-            totalValue = chipQuantity1 * chipAmount1
-            totalValue = (totalValue) + chipQuantity2 * chipAmount2
-            totalValue = (totalValue) + chipQuantity3 * chipAmount3
-            totalValue = (totalValue) + chipQuantity4 * chipAmount4
-            totalValue = (totalValue) + chipQuantity5 * chipAmount5
-            totalValue = (totalValue) + chipQuantity6 * chipAmount6
-            totalValue = (totalValue) + chipQuantity7 * chipAmount7
-            totalValue = (totalValue) + chipQuantity8 * chipAmount8
-            totalValue = totalValue / tPl
-            results.text = "\(chipQuantity1/tPl) \(chipName1.uppercased()) CHIPS EACH\n\(chipQuantity2/tPl) \(chipName2.uppercased()) CHIPS EACH\n\(chipQuantity3/tPl) \(chipName3.uppercased()) CHIPS EACH\n\(chipQuantity4/tPl) \(chipName4.uppercased()) CHIPS EACH\n\(chipQuantity5/tPl) \(chipName5.uppercased()) CHIPS EACH\n\(chipQuantity6/tPl) \(chipName6.uppercased()) CHIPS EACH\n\(chipQuantity7/tPl) \(chipName7.uppercased()) CHIPS EACH\n\(chipQuantity8/tPl) \(chipName8.uppercased()) CHIPS EACH \n\nINDIVIDUAL VALUE: $\(totalValue)"
+            var totalValue1:String = ""
+            totalValue1 = "CHIPS @ $\(chipAmount1) EACH"
+            var totalValue2:String = ""
+            totalValue2 = "CHIPS @ $\(chipAmount2) EACH"
+            var totalValue3:String = ""
+            totalValue3 = "CHIPS @ $\(chipAmount3) EACH"
+            var totalValue4:String = ""
+            totalValue4 = "CHIPS @ $\(chipAmount4) EACH"
+            var totalValue5:String = ""
+            totalValue5 = "CHIPS @ $\(chipAmount5) EACH"
+            var totalValue6:String = ""
+            totalValue6 = "CHIPS @ $\(chipAmount6) EACH"
+            var totalValue7:String = ""
+            totalValue7 = "CHIPS @ $\(chipAmount7) EACH"
+            var totalValue8:String = ""
+            totalValue8 = "CHIPS @ $\(chipAmount8) EACH"
+            results.text = "\(chipQuantity1/tPl) \(chipName1.uppercased()) \(totalValue1)\n\(chipQuantity2/tPl) \(chipName2.uppercased()) \(totalValue2)\n\(chipQuantity3/tPl) \(chipName3.uppercased()) \(totalValue3)\n\(chipQuantity4/tPl) \(chipName4.uppercased()) \(totalValue4)\n\(chipQuantity5/tPl) \(chipName5.uppercased()) \(totalValue5)\n\(chipQuantity6/tPl) \(chipName6.uppercased()) \(totalValue6)\n\(chipQuantity7/tPl) \(chipName7.uppercased()) \(totalValue7)\n\(chipQuantity8/tPl) \(chipName8.uppercased()) \(totalValue8)"
         }
         
     }
